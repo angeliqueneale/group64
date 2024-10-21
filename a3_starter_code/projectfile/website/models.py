5be2f14f46ac8c6e6ac258105a699cb6e0a65efd
lTimol <n11079517@qut.edu.au>
2024-10-21 19:56:55 +1000

    Home Page

--- a3_starter_code/projectfile/website/models.py
+++ a3_starter_code/projectfile/website/models.py
@@ -2,10 +2,48 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
+# User model
 class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    contact_number = db.Column(db.String(20), nullable=True)
+    street_address = db.Column(db.String(200), nullable=True)
 
+    def __repr__(self):
+        return f'<User {self.first_name} {self.last_name}>'
+
+# Event model
 class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(50), nullable=False)  # e.g., Open, Inactive, Sold Out, Cancelled
 
+    def __repr__(self):
+        return f'<Event {self.title}>'
+
+# Comment model
 class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return f'<Comment {self.content} on Event {self.event_id}>'
+
+# Order model
 class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    order_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Order {self.id} for Event {self.event_id}>'
